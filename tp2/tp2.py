710faf93051f92b68de696afbd8b956589e0be69
= <hassane.mamane017@gmail.com>
2024-12-07 10:10:30 +0100

    faire ensembles done

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -3,15 +3,6 @@ from classes.paire import Paire
 
 
 def lire_blocs(filepath):
-    """
-    Lit un fichier et retourne une liste de blocs (groupes de 14 lignes).
-
-    Args:
-        filepath (str): Le chemin vers le fichier à lire.
-
-    Returns:
-        list: Une liste de blocs, chaque bloc est une liste de lignes de vecteurs.
-    """
     try:
         with open(filepath, 'r', encoding='utf-8') as file:
             lines = file.readlines()
@@ -36,19 +27,9 @@ def lire_blocs(filepath):
 
 
 def traiter_bloc_en_paire(bloc):
-    """
-    Transforme un bloc en une paire de vecteurs et de classification.
-
-    Args:
-        bloc (list): Un bloc contenant 14 lignes (vecteurs, nom, classification).
-
-    Returns:
-        Paire: Une instance de `Paire` avec le vecteur et la classification.
-    """
     vecteur = []
-
     # Lire les lignes de vecteurs (2ème à 11ème)
-    for i in range(2, 12):  # Les premières 10 lignes contiennent les vecteurs
+    for i in range(1, 12):  # On commence à la 1ère ligne (1) jusqu'à la 11ème
         ligne_donnees = bloc[i].strip('{} \n')
         if ligne_donnees:  # Ignore les lignes vides
             valeurs = [float(val) for val in ligne_donnees.split()]
@@ -56,21 +37,12 @@ def traiter_bloc_en_paire(bloc):
 
     # La 13ème ligne contient la classification
     classification_str = bloc[12].strip()  # La ligne vide avant la classification
-    # print("classification str ",classification_str)
     classification = 1 if classification_str == '1' else -1
+
     return Paire(tuple(vecteur), classification)
 
 
 def construire_ensemble(filepath):
-    """
-    Crée un Ensemble à partir d'un fichier train ou test.
-
-    Args:
-        filepath (str): Le chemin vers le fichier à lire.
-
-    Returns:
-        Ensemble: Un objet `Ensemble` contenant toutes les paires lues depuis le fichier.
-    """
     ensemble = Ensemble()
     try:
         blocs = lire_blocs(filepath)
@@ -97,3 +69,21 @@ print("Train Ensemble contient :", len(train_ensemble.elements), "paires.")
 print("Test Ensemble contient :", len(test_ensemble.elements), "paires.")
 # print(train_ensemble)
 # print(test_ensemble)
+
+
+def creer_all_ensemble(train_filepath, test_filepath):
+    train_ensemble = construire_ensemble(train_filepath)
+    test_ensemble = construire_ensemble(test_filepath)
+
+    # Créer un nouvel ensemble qui combine les paires des deux ensembles
+    all_ensemble = Ensemble(*train_ensemble.elements, *test_ensemble.elements)
+
+    return all_ensemble
+
+
+# Exemple d'utilisation
+all_ensemble = creer_all_ensemble('tp2/result_sets/train_set.txt', 'tp2/result_sets/test_set.txt')
+
+# Affichage pour validation
+print("All Ensemble contient :", len(all_ensemble.elements), "paires.")
+print(all_ensemble)
